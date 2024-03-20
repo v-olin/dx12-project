f49cacf8dfc673cd4c80f7498b143b3462104eb8
Hannes Kaulio <hannes.kaulio@gmail.com>
2024-03-20 12:21:28 +0100

    Added classes

--- src/DXPipeline.h
+++ src/DXPipeline.h
@@ -1,7 +1,6 @@
 #pragma once
-
 namespace pathtracex {
-	class Material {
+	class DXPipeline {
 
-	};
+};
 }
--- src/Scene.h
+++ src/Scene.h
@@ -1,7 +1,10 @@
 #pragma once
+#include <vector>
+#include <memory>
+#include "Model.h"
 
 namespace pathtracex {
 	class Scene {
-
+		std::vector<std::shared_ptr<Model>> models;
 	};
 }
