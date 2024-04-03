66fa5385cc4ec961199d8821eb6be7971e1c3351
Hannes Kaulio <hannes.kaulio@gmail.com>
2024-04-03 13:36:16 +0200

    Added blinn-phong shading

--- shaders/PixelShader.hlsl
+++ shaders/PixelShader.hlsl
@@ -21,15 +21,30 @@ cbuffer ConstantBuffer : register(b0)
     int pointLightCount; // 4 bytes
 };
 
-float3 calculatePointLight(PointLight light, float3 normal, float3 position)
+float3 calculatePointLight(PointLight light, float3 normal, float3 position, float3 baseColor)
 {
     float3 lightDir = light.position - position;
     float distance = length(lightDir);
     lightDir /= distance;
     
-    float intensity = max(dot(lightDir, normal), 0.0f);
+    float3 diffuse = baseColor * max(dot(lightDir, normal), 0.0f);
     
-    return intensity;
+    // ambient light
+    float3 ambient = baseColor * 0.2f;
+    
+    // specular light
+    float3 viewDir = normalize(-position);
+    float3 reflectDir = reflect(-lightDir, normal);
+    float3 specular = baseColor * pow(max(dot(viewDir, reflectDir), 0.0f), 32.0f);
+    
+    // attenuation
+    float attenuation = 1.0f / (1.0f + 0.1f * distance + 0.01f * distance * distance);
+    
+    diffuse *= attenuation;
+//    ambient *= attenuation;
+    specular *= attenuation;
+    
+    return diffuse + ambient + specular;
 }
 
 float4 main(VS_OUTPUT input) : SV_TARGET
@@ -39,7 +54,12 @@ float4 main(VS_OUTPUT input) : SV_TARGET
         return input.color;
     }
     
-    float3 result = calculatePointLight(pointLights[0], input.worldNormal.xyz, input.worldPos.xyz);
+    float3 result = float3(0, 0, 0);
+    for (int i = 0; i < pointLightCount; i++)
+    {
+        result += calculatePointLight(pointLights[i], input.worldNormal.xyz, input.worldPos.xyz, input.color);
+    }
+
     float4 color = float4(result, 1.0f);
     // return interpolated color
     return color;
