8c45b1a62b7e065558a4c0e7018bfc42a72edf97
eliskleen <eliskleen00@gmail.com>
2024-04-16 18:32:11 +0200

    uncomment pbr direct lighting, vectors need fixing

--- shaders/PixelShader.hlsl
+++ shaders/PixelShader.hlsl
@@ -63,48 +63,48 @@ float point_light_intensity_multiplier = 50.0;
 float3 calculateDirectIllumiunation(float3 wo, float3 n, float3 baseCol, VS_OUTPUT input)
 {
     float direct_illum = float3(0, 0, 0);
-    //for (int i = 0; i < pointLightCount; i++)
-    //{
+    for (int i = 0; i < pointLightCount; i++)
+    {
         
-    //    PointLight light = pointLights[i];
-    //    float3 direct_illum = baseCol;
+        PointLight light = pointLights[i];
+        float3 direct_illum = baseCol;
 	
-    //    //float d = distance(viewSpaceLightPosition, viewSpacePosition);
-    //    float d = distance(light.position, input.pos);
+        //float d = distance(viewSpaceLightPosition, viewSpacePosition);
+        float d = distance(light.position, input.pos);
 
-    //    float3 Li = point_light_intensity_multiplier * point_light_color * (1 / (d * d));
+        float3 Li = point_light_intensity_multiplier * point_light_color * (1 / (d * d));
 
-    //    float3 wi = normalize(light.position - input.pos);
+        float3 wi = normalize(light.position - input.pos);
 	
-    //    float dotp = dot(n, wi);
+        float dotp = dot(n, wi);
 
-    //    if (dotp <= 0)
-    //        return float3(0.0f, 0.0, 0.0);
+        if (dotp <= 0)
+            return float3(0.0f, 0.0, 0.0);
 
-    //    float3 diffuseTerm = baseCol * 1 / PI * abs(dotp) * Li;
+        float3 diffuseTerm = baseCol * 1 / PI * abs(dotp) * Li;
 
-    //    float3 wh = normalize(wi + wo); // can be too short
-    //    float ndotwh = max(0.0001f, dot(n, wh));
-    //    float ndotwo = max(0.0001f, dot(n, wo));
-    //    float wodotwh = max(0.0001f, dot(wo, wh));
-    //    float ndotwi = max(0.0001f, dot(n, wi));
-    //    float whdotwi = max(0.0001f, dot(wh, wi));
+        float3 wh = normalize(wi + wo); // can be too short
+        float ndotwh = max(0.0001f, dot(n, wh));
+        float ndotwo = max(0.0001f, dot(n, wo));
+        float wodotwh = max(0.0001f, dot(wo, wh));
+        float ndotwi = max(0.0001f, dot(n, wi));
+        float whdotwi = max(0.0001f, dot(wh, wi));
         
-    //    float F = material_fresnel + (1 - material_fresnel) * pow(1 - whdotwi, 5);
-    //    float D = ((material_shininess + 2) / (2 * PI)) * pow(ndotwh, material_shininess);
+        float F = material_fresnel + (1 - material_fresnel) * pow(1 - whdotwi, 5);
+        float D = ((material_shininess + 2) / (2 * PI)) * pow(ndotwh, material_shininess);
         
-    //    float left = 2 * (ndotwh * ndotwo) / wodotwh;
-    //    float right = 2 * (ndotwh * ndotwi) / wodotwh;
-    //    float G = min(1, min(left, right));
+        float left = 2 * (ndotwh * ndotwo) / wodotwh;
+        float right = 2 * (ndotwh * ndotwi) / wodotwh;
+        float G = min(1, min(left, right));
 
-    //    float brdf = (F * D * G) / (4 * ndotwo * ndotwi);
+        float brdf = (F * D * G) / (4 * ndotwo * ndotwi);
 
-    //    float3 dielectric_term = brdf * ndotwi * Li + (1 - F) * diffuseTerm;
+        float3 dielectric_term = brdf * ndotwi * Li + (1 - F) * diffuseTerm;
 
-    //    float3 metal_term = brdf * baseCol * ndotwi * Li;
+        float3 metal_term = brdf * baseCol * ndotwi * Li;
 
-    //    direct_illum += (material_metalness * metal_term + (1 - material_metalness) * dielectric_term) / pointLightCount;
-    //}
+        direct_illum += (material_metalness * metal_term + (1 - material_metalness) * dielectric_term) / pointLightCount;
+    }
 
     return direct_illum;
 }
@@ -120,8 +120,10 @@ float4 main(VS_OUTPUT input) : SV_TARGET
     {
         result += calculatePointLight(pointLights[i], input.worldNormal.xyz, input.worldPos.xyz, input.color);
     }
+
     //float3 wo = -normalize(input.pos);
-    //float3 n = normalize(input.no);
+    //float3 n = normalize(input.worldNormal);
+    //result = calculateDirectIllumiunation(wo, n, input.color.xyz, input);
     //result = calculateDirectIllumiunation()
 
     float4 color = float4(result, 1.0f);
@@ -129,5 +131,5 @@ float4 main(VS_OUTPUT input) : SV_TARGET
     ////also sample texture if it exists, needs to be implemented and passed in
     if (hasColTex)
         return colTex.Sample(s1, input.texCoord);
-    return input.color;
+    return float4(result, 1);
 }
